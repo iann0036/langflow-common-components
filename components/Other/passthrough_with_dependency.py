16ab79122c7a926e3170de8df796688dfeb86f1a
Ian Mckay <contact@ianmckay.com.au>
2026-01-25 18:23:06 +1100

    Adjust type names for passthrough

--- components/Other/passthrough_with_dependency.py
+++ components/Other/passthrough_with_dependency.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 # pyright: reportCallIssue=false
-from typing import Any, cast
+from typing import Any, Text, TypeAlias, TypeVar
 from lfx.custom.custom_component.component import Component
 from lfx.io import HandleInput, DropdownInput, Output
 from lfx.schema import (
@@ -12,92 +12,88 @@ from lfx.schema import (
 from lfx.field_typing import (
     AgentExecutor,
     BaseChatMemory,
+    BaseChatMessageHistory,
     BaseChatModel,
     BaseDocumentCompressor,
     BaseLanguageModel,
     BaseLLM,
+    BaseLLMOutputParser,
     BaseLoader,
     BaseMemory,
     BaseOutputParser,
     BasePromptTemplate,
     BaseRetriever,
+    BaseTool,
     Callable,
     Chain,
     ChatPromptTemplate,
     Code,
     Document,
     Embeddings,
-    #LanguageModel,
-    NestedDict,
     Object,
     PromptTemplate,
-    #Retriever,
     Text,
     TextSplitter,
     Tool,
     VectorStore,
+    VectorStoreRetriever,
+) # https://github.com/langflow-ai/langflow/blob/main/src/backend/base/langflow/field_typing/constants.py
+from lfx.field_typing.constants import (
+    LanguageModel,
+    Memory,
+    NestedDict,
+    OutputParser,
+    Retriever,
+    ToolEnabledLanguageModel,
 ) # https://github.com/langflow-ai/langflow/blob/main/src/lfx/src/lfx/field_typing/constants.py
-
-ALL_INPUT_TYPES = [
-    "AgentExecutor",
-    "BaseChatMemory",
-    "BaseChatModel",
-    "BaseDocumentCompressor",
-    "BaseLanguageModel",
-    "BaseLLM",
-    "BaseLoader",
-    "BaseMemory",
-    "BaseOutputParser",
-    "BasePromptTemplate",
-    "BaseRetriever",
-    "Callable",
-    "Chain",
-    "ChatPromptTemplate",
-    "Code",
-    "Data",
-    "DataFrame",
-    "Document",
-    "Embeddings",
-    "Message",
-    "NestedDict",
-    "Object",
-    "PromptTemplate",
-    "Text",
-    "TextSplitter",
-    "Tool",
-    "VectorStore"
-]
 
 # Mapping of type names to their actual type objects
 TYPE_MAP = {
     "AgentExecutor": AgentExecutor,
     "BaseChatMemory": BaseChatMemory,
+    "BaseChatMessageHistory": BaseChatMessageHistory,
     "BaseChatModel": BaseChatModel,
     "BaseDocumentCompressor": BaseDocumentCompressor,
-    "BaseLanguageModel": BaseLanguageModel,
     "BaseLLM": BaseLLM,
+    "BaseLLMOutputParser": BaseLLMOutputParser,
+    "BaseLanguageModel": BaseLanguageModel,
     "BaseLoader": BaseLoader,
     "BaseMemory": BaseMemory,
     "BaseOutputParser": BaseOutputParser,
     "BasePromptTemplate": BasePromptTemplate,
     "BaseRetriever": BaseRetriever,
+    "BaseTool": BaseTool,
     "Callable": Callable,
     "Chain": Chain,
+    "ChatMemory": BaseChatMemory,
+    "ChatModel": BaseChatModel,
     "ChatPromptTemplate": ChatPromptTemplate,
     "Code": Code,
     "Data": Data,
     "DataFrame": DataFrame,
     "Document": Document,
+    "DocumentCompressor": BaseDocumentCompressor,
     "Embeddings": Embeddings,
+    "LanguageModel": LanguageModel,
+    "LLM": BaseLLM,
+    "Loader": BaseLoader,
+    "Memory": Memory,
     "Message": Message,
     "NestedDict": NestedDict,
     "Object": Object,
+    "OutputParser": OutputParser,
     "PromptTemplate": PromptTemplate,
+    "Retriever": Retriever,
     "Text": Text,
     "TextSplitter": TextSplitter,
     "Tool": Tool,
+    "ToolEnabledLanguageModel": ToolEnabledLanguageModel,
     "VectorStore": VectorStore,
+    "VectorStoreRetriever": VectorStoreRetriever,
 }
+
+# Derive ALL_INPUT_TYPES from TYPE_MAP keys
+ALL_INPUT_TYPES = list(TYPE_MAP.keys())
 
 
 class PassthroughDependency(Component):
@@ -209,36 +205,6 @@ class PassthroughDependency(Component):
     def passthrough_input_agentexecutor(self) -> AgentExecutor:
         return self.original_input
     
-    def passthrough_input_basechatmemory(self) -> BaseChatMemory:
-        return self.original_input
-    
-    def passthrough_input_basechatmodel(self) -> BaseChatModel:
-        return self.original_input
-    
-    def passthrough_input_basedocumentcompressor(self) -> BaseDocumentCompressor:
-        return self.original_input
-    
-    def passthrough_input_baselanguagemodel(self) -> BaseLanguageModel:
-        return self.original_input
-    
-    def passthrough_input_basellm(self) -> BaseLLM:
-        return self.original_input
-    
-    def passthrough_input_baseloader(self) -> BaseLoader:
-        return self.original_input
-    
-    def passthrough_input_basememory(self) -> BaseMemory:
-        return self.original_input
-    
-    def passthrough_input_baseoutputparser(self) -> BaseOutputParser:
-        return self.original_input
-    
-    def passthrough_input_baseprompttemplate(self) -> BasePromptTemplate:
-        return self.original_input
-    
-    def passthrough_input_baseretriever(self) -> BaseRetriever:
-        return self.original_input
-    
     def passthrough_input_callable(self) -> Callable:
         return self.original_input
     
@@ -285,5 +251,77 @@ class PassthroughDependency(Component):
         return self.original_input
     
     def passthrough_input_vectorstore(self) -> VectorStore:
+        return self.original_input
+    
+    def passthrough_input_basechatmemory(self) -> BaseChatMemory:
+        return self.original_input
+    
+    def passthrough_input_basechatmessagehistory(self) -> BaseChatMessageHistory:
+        return self.original_input
+    
+    def passthrough_input_basechatmodel(self) -> BaseChatModel:
+        return self.original_input
+    
+    def passthrough_input_basedocumentcompressor(self) -> BaseDocumentCompressor:
+        return self.original_input
+    
+    def passthrough_input_basellm(self) -> BaseLLM:
+        return self.original_input
+    
+    def passthrough_input_basellmoutputparser(self) -> BaseLLMOutputParser:
+        return self.original_input
+    
+    def passthrough_input_baselanguagemodel(self) -> BaseLanguageModel:
+        return self.original_input
+    
+    def passthrough_input_baseloader(self) -> BaseLoader:
+        return self.original_input
+    
+    def passthrough_input_basememory(self) -> BaseMemory:
+        return self.original_input
+    
+    def passthrough_input_baseoutputparser(self) -> BaseOutputParser:
+        return self.original_input
+    
+    def passthrough_input_baseprompttemplate(self) -> BasePromptTemplate:
+        return self.original_input
+    
+    def passthrough_input_baseretriever(self) -> BaseRetriever:
+        return self.original_input
+    
+    def passthrough_input_basetool(self) -> BaseTool:
+        return self.original_input
+    
+    def passthrough_input_chatmemory(self) -> BaseChatMemory:
+        return self.original_input
+    
+    def passthrough_input_chatmodel(self) -> BaseChatModel:
+        return self.original_input
+    
+    def passthrough_input_documentcompressor(self) -> BaseDocumentCompressor:
+        return self.original_input
+    
+    def passthrough_input_languagemodel(self) -> LanguageModel:
+        return self.original_input
+    
+    def passthrough_input_llm(self) -> BaseLLM:
+        return self.original_input
+    
+    def passthrough_input_loader(self) -> BaseLoader:
+        return self.original_input
+    
+    def passthrough_input_memory(self) -> Memory:
+        return self.original_input
+    
+    def passthrough_input_outputparser(self) -> OutputParser:
+        return self.original_input
+    
+    def passthrough_input_retriever(self) -> Retriever:
+        return self.original_input
+    
+    def passthrough_input_toolenabledlanguagemodel(self) -> ToolEnabledLanguageModel:
+        return self.original_input
+    
+    def passthrough_input_vectorstoreretriever(self) -> VectorStoreRetriever:
         return self.original_input
     
