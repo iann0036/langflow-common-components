2478e7de62c921d334783722beef85bb22f5d12b
Ian Mckay <contact@ianmckay.com.au>
2026-01-27 21:49:51 +1100

    Adjust init

--- components/Other/__init__.py
+++ components/Other/__init__.py
@@ -1,3 +1,5 @@
 from .slack_message import SlackMessage
+from .aws_api_call import AWSAPICallComponent
+from .passthrough_with_dependency import PassthroughDependency
 
-__all__ = ["SlackMessage"]
+__all__ = ["SlackMessage", "AWSAPICallComponent", "PassthroughDependency"]
